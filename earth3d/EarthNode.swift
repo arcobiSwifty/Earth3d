150e7e6fc8c1631b2199c5f6d9cb67f606c48374
arcobiSwifty <arcobi98@gmail.com>
2018-02-01 15:58:39 +0100

    first Commit

---
+++ earth3d/EarthNode.swift
@@ -0,0 +1,13 @@
+//
+//  EarthNode.swift
+//  earth3d
+//
+//  Created by Vincent Bossaerts on 04/12/17.
+//  Copyright © 2017 Vincent Bossaerts. All rights reserved.
+//
+
+import UIKit
+
+class EarthNode: SCNNode {
+
+}
